73fe9690699728030d357fff2f4e8aaf94e51071
Davis Esmael <96700031+DavisEsmael@users.noreply.github.com>
2025-08-25 21:04:33 -0300

    feat: remove syle and add cv form

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  return(
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Gerador de Curriculos AI</h1>
+        <p>Gerador inteligente de Currículos com IA</p>
+      </header>
+
+      <main>
+        {/* card da esquerda no qual o usuário preenche os dados */}
+        <div>
+          <section>
+            <h2>Informações do Currículo</h2>
+            <p>Preencha os dados e veja o previwe em tempo real</p>
+          </section>
+
+          <section>
+
+            <h3>Dados pessoais</h3>
+            <p>Informações básicas para contato</p>
+
+            <form action="">
+              <div>
+                <label htmlFor="fullName">Nome Completo</label>
+                <br />
+                <input type="text" name="fullName" id="fullName" placeholder='Seu nome completo'/>
+              </div>
+
+              <div>
+                <label htmlFor="email">Email</label>
+                <br />
+                <input type="email" name="email" id="email" placeholder='seu.email@exemplo.com' />
+              </div>
+
+              <div>
+                <label htmlFor="phone">Telefone</label>
+                <br />
+                <input type="tel" name="phone" id="phone" placeholder='(11) 99999-9999' />
+              </div>
+
+              <div>
+                <label htmlFor="linkedin">LinkedIn</label>
+                <br />
+                <input type="text" name="linkedin" id="linkedin" placeholder='linkedin.com/in/seuperfil' />
+              </div>
+
+            </form>
+
+          </section>          
+          
+        </div>
+
+        {/* card da direita no qual o usuário visualiza o preview */}
+        <div>
+          <h2>Preview do Curriculo</h2>
+          <p>Visualização em tempo real</p>
+        </div>
+
+
+      </main>
     </>
-  )
+  )  
 }
 
 export default App
